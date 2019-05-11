56556196b18170aebc5f79cbdaad68b1ccc82c63
youngwoo <dbsduddn208@gmail.com>
2019-05-11 06:31:31 +0000

    creator 모델링 추가

--- movies/models.py
+++ movies/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Creator(models.Model): # 감독과 배우 모두 들어있는 클래스
+    name = models.TextField()
+    
 class Genre(models.Model):
     name = models.TextField()
     
-# class Actor(models.Model):
-    # actor_id = models.IntegerField()
-    
 class Movie(models.Model):
-    movie_id = models.IntegerField()
     title = models.TextField()
     original_title = models.TextField()
     runtime = models.IntegerField()
@@ -16,5 +15,5 @@ class Movie(models.Model):
     overview = models.TextField()
     poster_url = models.TextField()
     popularity = models.IntegerField()
-    # actors = models.ManyToManyField(Actor, related_name='movies')
-    director = models.TextField()
+    actors = models.ManyToManyField(Creator, related_name='movies')
+    director = models.ForeignKey(Creator, on_delete=models.CASCADE)
