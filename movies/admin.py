5b993c77aa59017b780e248a4384608b6d9f9e88
youngwoo <dbsduddn208@gmail.com>
2019-05-13 09:03:35 +0000

    0513|예고편 크롤링 완료

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Genre, Creator
+from .models import Movie, Genre, Creator, Score, Video
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
@@ -10,6 +10,13 @@ class GenreAdmin(admin.ModelAdmin):
 class CreatorAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'value']
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'key', 'name']
+
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Creator, CreatorAdmin)
+admin.site.register(Video, VideoAdmin)
